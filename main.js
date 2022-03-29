48718ce4823d2a90b7dd17d9c71a4a0968eed3c8
MatthewCodes123 <84956326+MatthewCodes123@users.noreply.github.com>
2022-03-29 21:59:06 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+status="";
+inputs="";
+
+function setup(){
+    canvas = createCanvas(460,380)
+    canvas.center()
+    video=createCapture(VIDEO)
+    video.hide()
+}
+
+function start(){
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded)
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+    inputs = document.getElementById("input").value
+}
+
+function modelLoaded(){
+    console.log("Model Loaded")
+    status=true
+}
+
+function draw(){
+    image(video,0,0,460,380)
+}
